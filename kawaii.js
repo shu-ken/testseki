a77847783d9dc084c3800ef9186e15f41e491c28
HANRI <76136252+shu-ken@users.noreply.github.com>
2023-05-20 13:54:53 +0900

    slider実装

--- kawaii.js
+++ kawaii.js
@@ -39,3 +39,10 @@ $(window).on('load', function () {
 
 	EachTextAnimeControl() /* アニメーション用の関数を呼ぶ*/
 }) // ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	fadeAnime() /* アニメーション用の関数を呼ぶ*/
+	header_area_fadeInAnime()
+	hanabira1_fadeInAnime()
+}) // ここまで画面をスクロールをしたら動かしたい場合の記述
